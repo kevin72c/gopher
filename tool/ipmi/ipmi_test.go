984d8809386b1b8e93188f8f361d8704a306c460
chenjw <chenjw@zimo.com>
2021-03-09 14:07:22 +0800

    .

--- tool/ipmi/ipmi_test.go
+++ tool/ipmi/ipmi_test.go
@@ -3,19 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"gopher/tool/ipmi/util"
 	"log"
+	"testing"
 )
 
 var c = flag.String("c", "x", "环境参数")
 
-func main() {
+func TestIPMIServer_Query(t *testing.T) {
 	flag.Parse()
 	fmt.Printf("command:%s\n", *c)
 
 	log.Printf("We are about to restart IPMI with this awesome package !")
 
-	server := util.IPMIServer{
+	server := IPMIServer{
 		Address:  "192.168.180.45",
 		User:     "admin",
 		Password: "12345678",
